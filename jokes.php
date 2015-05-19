6c8fe78f96530b3b460a6f0c9a4c242753fd78b6
q_hiep <q_hiep@HDQHiep-PC.brycen-vn.local>
2015-05-19 11:22:29 +0700

    Merge origin/master
    Conflicts:
    	getapp.php
    	nbproject/project.xml

--- jokes.php
+++ jokes.php
@@ -1,8 +1,26 @@
 <?php
+include "simple_html_dom.php";
 //"http://www.manythings.org/jokes/"
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$url = "http://www.manythings.org/jokes/";
+$url_audio = "http://www.manythings.org/jokes/9962.html";
+var_dump(getAudio($url_audio));
+function getUrl($url){
+    $html = file_get_html($url);
+    $string = array();
+    $string1 = array();
+    foreach($html->find('a') as $element) {
+       array_push($string, array("http://www.manythings.org/jokes/".$element->href,$element->innertext));
+    }
+    unset($string[38]);
+    unset($string[39]);
+    unset($string[40]);
+    return $string;
+}
+function getAudio($url){
+    $file = file_get_html($url);
+    $tmp_tra_loi = $file->find("div[class='content_main']");
+
+   
+    return $tmp_tra_loi;
+}
 
