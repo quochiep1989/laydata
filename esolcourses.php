93ffe90230d0fbac8368478eb2a3599a87d45da1
q_hiep <q_hiep@HDQHiep-PC.brycen-vn.local>
2015-06-17 14:23:01 +0700

    Merge origin/master
    Conflicts:
    	getapp.php
    	nbproject/project.xml

--- esolcourses.php
+++ esolcourses.php
@@ -4,7 +4,11 @@ include "simple_html_dom.php";
 $page = "http://www.englishspeak.com/english-lessons.cfm";
 
 $a = getUrl($page);
-var_dump(getContent($a[0][0]));
+
+foreach ($a as $i){
+    //var_dump($i);
+    var_dump(getContent($i[0]));
+}
 function getUrl($url){
     $html = file_get_html($url);
     $string = array();
@@ -19,13 +23,33 @@ function getUrl($url){
 function getContent($url_page){
     $html = file_get_html($url_page);
     $string = array();
-    foreach($html->find('table[class="blacktext"]') as $element) {
-       array_push($string, array($element->innertext));
+    $data = array();
+    $tmp = array();
+    foreach($html->find('table[class="blacktext"] tbody table tr') as $element) {
+        $string1 = "";
+        $string2 = "";
+        //array_push($string, array($element->innertext));
+        $html = str_get_html($element->innertext);
+        $content1 = $html->find('a');
+        $content2 = $html->find('img');
+        foreach ($content1 as $key=>$i){
+            if($key==0){
+                $string1 = $i->innertext.":";
+            }else{
+                $string1 = $string1 ." ". $i->innertext;
+            }
+        }
+        foreach ($content2 as $i){
+            $string2 = $i->onclick;
+            $string2= str_replace("javascript:playMp3('","", $string2);
+            $string2= str_replace("')","", $string2);
+        }
+        if(!empty($string1)){
+            array_push($tmp,array(strip_tags($string1,'span'),$string2)); 
+            array_push($data, $tmp);
+            $tmp = array();
+        }
     }
-    $string = explode("</tr>",$string[1][0]);
-    foreach ($string as $line){
-        $line  = str_replace('<tr class = "DataB">',"",$line);
-        $line  = str_replace('<tr class = "DataA">',"",$line);
-        
-    }
+    return $data;
+   
 }
