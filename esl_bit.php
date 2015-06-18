74060e89dafe01b67b7ad74414a070715d070d5b
q_hiep <q_hiep@HDQHiep-PC.brycen-vn.local>
2015-06-18 15:31:43 +0700

    Merge origin/master
    Conflicts:
    	getapp.php
    	nbproject/project.xml

--- esl_bit.php
+++ esl_bit.php
@@ -1,20 +1,48 @@
 <?php
+
 include "simple_html_dom.php";
+$page = "http://esl-bits.net/idioms/id4.htm";
+var_dump(getContent($page));
 
-for($i=65;$i<=90;$i++){
-    $url = "http://esl-bits.net/idioms/idTit".chr($i).".htm";
-    var_dump (getUrl($url,chr($i)));
-}
+//for($i=65;$i<=90;$i++){
+//    $url = "http://esl-bits.net/idioms/idTit".chr($i).".htm";
+//    var_dump (getUrl($url,chr($i)));
+//    
+//}
 
-function getUrl($url,$key_name){
+function getUrl($url, $key_name) {
     $html = file_get_html($url);
     $string = array();
     $string1 = array();
-    foreach($html->find('table a') as $key=>$element) {    
-        array_push($string, array($key_name,"http://esl-bits.net/idioms/".$element->href,$element->innertext));
+    foreach ($html->find('table a') as $key => $element) {
+        array_push($string, array($key_name, "http://esl-bits.net/idioms/" . $element->href, $element->innertext));
     }
     return $string;
 }
-function getContent($url){
-    
+
+function getContent($url) {
+    $html = file_get_html($url);
+    $string = "";
+    $array = array();
+    $tmp = array();
+    foreach ($html->find('table tr') as $element) {
+        $html = str_get_html($element->innertext);
+        $content1 = $html->find('td');
+        foreach ($content1 as $key=>$i) {
+            if($key == 0){
+                $tmp["idiom"] = $i;
+            }
+            else if($key == 1){
+                $tmp['mean'] = $i;
+            }else{
+                $tmp['exam'] = $i;
+            }
+            //$string = $string . $i;
+        }
+        array_push($array, $tmp);
+        //$string = "";
+        $tmp = array();
+    }
+    unset($array[0]);
+    return $array;
 }
